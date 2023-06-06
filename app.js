8a2b155b16d9ee12eb4cb1de9125919c64ff6826
JorgeB.Monroy <bobadillamonroy27@gmail.com>
2023-06-06 17:08:45 -0600

    Feature(app - configs/mongo): * Se añade archivo de configuraciones de mongo, se crea función dbConnect para la conexión a la base de datos en 'Mongo Atlas'. * Se establece la conexión en el archivo app.js mediante la función dbConnect.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const dbConnect = require('./configs/mongo-configs');
 
 const app = express();
 const PORT = process.env.PORT;
@@ -12,3 +13,5 @@ app.use('/api', require('./routes'));
 app.listen(PORT, () => {
     console.log(`*** Listen on PORT: ${PORT} ***`);
 });
+
+dbConnect();

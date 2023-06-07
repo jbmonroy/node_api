54df20d08c05ba8e47539efd203a6a3b527a039f
JorgeB.Monroy <bobadillamonroy27@gmail.com>
2023-06-07 11:19:18 -0600

    Feature(storages): * Se terminan las rutas y controllers para storages.

--- routes/storages-routing.js
+++ routes/storages-routing.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const uploadMiddleware = require('../utils/handleStorage');
-const { postFile } = require('../controllers/storages-controller');
+const { postFile, getFile, deleteFile } = require('../controllers/storages-controller');
+const { idValidator } = require('../validators/commonValidators');
 
 const router = express.Router();
 
+router.get('/:id', idValidator, getFile);
 router.post('/', uploadMiddleware.single('file'), postFile);
+router.delete('/:id', idValidator, deleteFile);
 
 module.exports = router;
